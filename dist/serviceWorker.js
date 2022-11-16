483c1b2e2e31c0e7044221d762f2ca7515314151
anijit <anijitsau@gmail.com>
2022-11-16 18:03:13 +0530

    await in service worker fix

--- dist/serviceWorker.js
+++ dist/serviceWorker.js
@@ -1,5 +1,14 @@
 const cacheName = "dashboard-page";
 
+async function installEventHandler() {
+  let cachedVal = await caches.open(cacheName);
+  return cachedVal.addAll(["/"]);
+}
+// install event
+self.addEventListener("install", async (installEvent) => {
+  installEvent.waitUntil(installEventHandler());
+});
+
 self.addEventListener("fetch", (fetchEvent) => {
   console.log("url of the req", fetchEvent.request.url);
   fetchEvent.respondWith(
