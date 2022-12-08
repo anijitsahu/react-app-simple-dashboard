274d73cb5d29284301adb62432552c2534b4908c
anijit <anijitsau@gmail.com>
2022-12-08 13:22:03 +0530

    timeline fixed

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -2,97 +2,65 @@
 import "../css/timeline.css";
 
 export default function Timeline() {
+  const timelineActions = [
+    {
+      timeframe: "8:30AM",
+      programList: [
+        { programName: "Science of Stupid", channelName: "Nat Geo" },
+        { programName: "Godhuli Aalaap", channelName: "Star Jalsha" },
+        { programName: "Man vs Wild", channelName: "Discovery" },
+      ],
+    },
+    {
+      timeframe: "12:30PM",
+      programList: [
+        { programName: "S.O.S Cars", channelName: "Nat Geo" },
+        { programName: "Dhulokona", channelName: "Star Jalsha" },
+        { programName: "India vs Newzealand", channelName: "Star Sports" },
+      ],
+    },
+    {
+      timeframe: "4:30PM",
+      programList: [
+        { programName: "Man vs Wild", channelName: "Discovery" },
+        { programName: "Wicked Tuna", channelName: "Nat Geo" },
+      ],
+    },
+    {
+      timeframe: "8:30PM",
+      programList: [
+        { programName: "To Catch a Smuggler", channelName: "Nat Geo" },
+        { programName: "Gaatchora", channelName: "Star Jalsha" },
+      ],
+    },
+  ];
   return (
     <div className="timeline-container">
       <section className="section">
         <div className="viewers-panel-title timeline-title">
           TV Watching Guide
         </div>
-
-        <div className="timeline-div">
-          <div className="timeframe">8:30AM</div>
-          <div className="actions">
-            <ul>
-              <li>
-                Science of Stupid{" "}
-                <span className="channel-metadata">(Nat Geo)</span>
-              </li>
-              <li>
-                Godhuli Aalaap{" "}
-                <span className="channel-metadata">(Star Jalsha)</span>
-              </li>
-              <li>
-                Man vs Wild{" "}
-                <span className="channel-metadata">(Discovery)</span>
-              </li>
-            </ul>
-          </div>
-        </div>
-
-        <div className="timeline-div">
-          <div className="timeframe">12:30PM</div>
-          <div className="actions">
-            <ul>
-              <li>
-                S.O.S cars <span className="channel-metadata">(Nat Geo)</span>
-              </li>
-              <li>
-                Dhulokona{" "}
-                <span className="channel-metadata">(Star Jalsha)</span>
-              </li>
-              <li>
-                India vs Newzealand{" "}
-                <span className="channel-metadata">(Star Sports)</span>
-              </li>
-            </ul>
-          </div>
-        </div>
-
-        <div className="timeline-div">
-          <div className="timeframe">4:30PM</div>
-          <div className="actions">
-            <ul>
-              <li>
-                Man vs Wild{" "}
-                <span className="channel-metadata">(Discovery)</span>
-              </li>
-              <li>
-                Wicked Tuna <span className="channel-metadata">(Nat Geo)</span>
-              </li>
-            </ul>
-          </div>
-        </div>
-
-        <div className="timeline-div">
-          <div className="timeframe">8:30PM</div>
-          <div className="actions">
-            <ul>
-              <li>
-                To Catch a Smuggler{" "}
-                <span className="channel-metadata">(Nat Geo)</span>
-              </li>
-              <li>
-                Gaatchora <span className="channel-metadata">(Star Plus)</span>
-              </li>
-            </ul>
-          </div>
-        </div>
-
-        <div className="timeline-div">
-          <div className="timeframe">12:30AM</div>
-          <div className="actions">
-            <ul>
-              <li>
-                Science of Stupid{" "}
-                <span className="channel-metadata">(Nat Geo)</span>
-              </li>
-              <li>
-                Air Crash Investigation{" "}
-                <span className="channel-metadata">(Discovery)</span>
-              </li>
-            </ul>
-          </div>
-        </div>
+        {timelineActions.map((timeline) => {
+          return (
+            <div className="timeline-div">
+              <div className="timeframe">{timeline.timeframe}</div>
+              <div className="actions">
+                <ul>
+                  {timeline.programList.map((program) => {
+                    return (
+                      <li>
+                        {program.programName}
+                        <span className="channel-metadata">
+                          ({program.channelName})
+                        </span>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </div>
+            </div>
+          );
+        })}
       </section>
     </div>
   );
